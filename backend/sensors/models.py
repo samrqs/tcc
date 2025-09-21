b53163c669fe7d825efc0088a8bdcfc3c7eb7f81
Samara Marques <samarassk@gmail.com>
2025-09-21 16:19:03 -0300

    feat: sensors data models

--- backend/sensors/models.py
+++ backend/sensors/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class SensorData(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sensor_data"
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    umidade = models.FloatField()
+    condutividade = models.FloatField()
+    temperatura = models.FloatField()
+    ph = models.FloatField()
+    nitrogenio = models.FloatField()
+    fosforo = models.FloatField()
+    potassio = models.FloatField()
+    salinidade = models.FloatField()
+    tds = models.FloatField()
+
+    def __str__(self):
+        return f"SensorData {self.id} - {self.user.email}"
